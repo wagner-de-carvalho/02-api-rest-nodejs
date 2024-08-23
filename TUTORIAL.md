e36c3a84df6f31a695f31e3905374ab1bef76af2
wagner carvalho <wagncarv@outlook.com>
2024-08-23 18:11:26 -0300

    feature: creating queries with knex

---
+++ TUTORIAL.md
@@ -0,0 +1,6 @@
+### knex
+
+- npm run knex -- migrate:latest = executa migration
+- npm run knex -- migrate:rollback = desfaz execução de migration
+- npm run knex -- migrate:make my-table-migration = cria migration
+- npm run knex -- -h = exibe informações do knex
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 /* eslint-disable prettier/prettier */
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
+    const transaction = await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title: 'Test transaction',
+        amount: 1000,
+    }).returning('*')
 
-    return tables
+    return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
