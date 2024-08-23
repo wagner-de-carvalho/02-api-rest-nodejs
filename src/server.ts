f72ebcff563a89217c9dafa37b6bfb42eec1953c
wagner carvalho <wagncarv@outlook.com>
2024-08-23 01:34:01 -0300

    feature: create application nodejs with fastify

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,7 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.get("/hello", () => { return "Hello node" })
+
 app.listen({ port: 3333 })
     .then(() => { console.log('HTTP server running ...') })
