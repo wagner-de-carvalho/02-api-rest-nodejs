2ae4e5a4ef486b9cbc83bce0cd2b3c6f73c9097a
wagner carvalho <wagncarv@outlook.com>
2024-08-23 01:47:22 -0300

    config: lint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import fastify from 'fastify'
 
 const app = fastify()
 
-app.get("/hello", () => { return "Hello node" })
+app.get('/hello', () => {
+    return 'Hello node'
+})
 
-app.listen({ port: 3333 })
-    .then(() => { console.log('HTTP server running ...') })
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP server running ...')
+})
