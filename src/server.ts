a874a143a911fb0e513a276cf4376b48adc1b62c
wagner carvalho <wagncarv@outlook.com>
2024-08-23 21:52:22 -0300

    feature: fastify plugins

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 /* eslint-disable prettier/prettier */
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    const transaction = await knex('transactions').insert({
-        id: crypto.randomUUID(),
-        title: 'Test transaction',
-        amount: 1000,
-    }).returning('*')
-
-    return transaction
-})
+app.register(transactionsRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP server running ...')
